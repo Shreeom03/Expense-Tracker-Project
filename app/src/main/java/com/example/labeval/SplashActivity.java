fe39e698bf70692e52e4b3b9eaf5a7ed7d91aeac
Shreeom03 <sharmashreeom8@gmail.com>
2024-11-09 10:00:12 +0530

    Adding Flash Screen

---
+++ app/src/main/java/com/example/labeval/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.example.labeval;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    private static final int SPLASH_DURATION = 2000; // 2 seconds
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash); // Link the splash screen layout
+
+        // Delay and move to Home Activity
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_DURATION);
+    }
+}
